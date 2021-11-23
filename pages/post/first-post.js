e7f0801cafe045017d8755dfb0328dd93e876cd5
Stepas Loiba <golfas705@yahoo.com>
2021-11-23 21:36:22 +0000

    changes 1.0.0
    added components folder and pictures

--- pages/post/first-post.js
+++ pages/post/first-post.js
@@ -1,14 +1,25 @@
 import Link from "next/link";
+import Image from "next/image";
+import Head from 'next/head';
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
+      <Image
+        src="/images/john-towner-3Kv48NS4WUU-unsplash.jpg"
+        height={200}
+        width={500}
+        alt="my picture"
+      />
     </>
   );
 }
